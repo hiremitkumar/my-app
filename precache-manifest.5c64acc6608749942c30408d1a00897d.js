2e007b9f3041c998f0a8e9f5aca90648e371ea62
Hire Mitkumar Kiranbhai <hiremitkumar@users.noreply.github.com>
2024-03-30 06:37:51 +0530

    Updates

---
+++ precache-manifest.5c64acc6608749942c30408d1a00897d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7036377355e802ede1684c452cdc3883",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ef35f6ca659c2ca375cb",
+    "url": "/my-app/static/css/main.fd726286.chunk.css"
+  },
+  {
+    "revision": "e75bc95b27ff28be00c6",
+    "url": "/my-app/static/js/2.e618a67e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/my-app/static/js/2.e618a67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a305264669ab0698eb9",
+    "url": "/my-app/static/js/3.a8bd2e92.chunk.js"
+  },
+  {
+    "revision": "ef35f6ca659c2ca375cb",
+    "url": "/my-app/static/js/main.c27da087.chunk.js"
+  },
+  {
+    "revision": "268e9915b5cad5b112be",
+    "url": "/my-app/static/js/runtime-main.245f70bb.js"
+  }
+]);
